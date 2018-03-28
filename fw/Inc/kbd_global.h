9063f811492a05566db09bc62498aa7049b26579
andreil <andreil499@gmail.com>
2018-03-28 16:31:13 +0300

    Схема обновлена до актуальной версии.

--- fw/Inc/kbd_global.h
+++ fw/Inc/kbd_global.h
@@ -15,14 +15,12 @@ extern uint16_t kbd_data[KBD_MATRIX_ROW];
 #define SW_MODE_RK86		0x01
 #define SW_MODE_MC7007	0x02
 
-#define SW_MODE_PS2			0x01
-#define SW_MODE_MATRIX	0x02
-#define SW_MODE_ZXBUS		0x04
-#define SW_MODE_RES1		0x08
-#define SW_MODE_RES2		0x10
-#define SW_MODE_RES3		0x20
-#define SW_MODE_RES4		0x40
-#define SW_MODE_RES5		0x80
+#define SW_MODE_PS2			0x01	// BIT 0
+#define SW_MODE_MATRIX	0x02	// BIT 1
+#define SW_MODE_ZXBUS		0x04	// BIT 2
+#define SW_MODE_RES1		0x08	// BIT 3
+#define SW_MODE_RES2		0x10	// BIT 4
+#define SW_MODE_RES3		0x20	// BIT 5
 
 extern uint8_t usb_mode;
 extern uint8_t PS2_SendRequest;
