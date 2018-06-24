8e67b91e77b3a0af3e40d581e3e43dd469ce3cd5
andreil <andreil499@gmail.com>
2018-06-24 20:39:37 +0300

    Обновлена процедура обновления прошивки - буффер во второй половине FLASH

--- fw/Src/fw_updater.c
+++ fw/Src/fw_updater.c
@@ -10,10 +10,14 @@
 #define LED_3_ON()    	LED3_GPIO_Port->BSRR = (LED3_Pin)
 #define LED_3_OFF()    	LED3_GPIO_Port->BSRR = (LED3_Pin << GPIO_BSRR_BR0_Pos)
 
-#define FW_MAX_SIZE (90*1024)
-__ALIGN_BEGIN uint8_t fw_buf[FW_MAX_SIZE] __ALIGN_END;
-uint32_t fw_size;
+#define FW_MAX_SIZE (256*1024)
 static uint32_t crc32r_table[256];
+uint32_t fw_size;
+uint8_t fw_read_buf[1024];
+
+#define FW_NEW_SECT_START (FLASH_SECTOR_6)
+#define FW_NEW_SECT_COUNT (2)
+#define FW_NEW_ADDR_START (FLASH_BASE + 0X00040000)
 
 #define CRC32_POLY_R 0xEDB88320
 
@@ -44,6 +48,39 @@ uint32_t crc32_byte(uint32_t init_crc, uint8_t *buf, int len)
 	return ~crc;
 }
 
+void prepare_new_fw_region(void)
+{
+	FLASH_EraseInitTypeDef EraseInitStruct;
+	uint32_t error_sector_num = 0;
+  EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+  EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+  EraseInitStruct.Sector = FW_NEW_SECT_START;
+	EraseInitStruct.NbSectors = FW_NEW_SECT_COUNT;
+	HAL_FLASH_Unlock();
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
+	HAL_FLASHEx_Erase(&EraseInitStruct, &error_sector_num);
+	FLASH_WaitForLastOperation(1000);
+	HAL_FLASH_Lock();
+}
+
+inline void flash_write(uint8_t* buf, uint32_t size, uint32_t start_addr)
+{
+	uint32_t wr_addr_start = start_addr;
+	uint32_t fw_offs = 0;
+	
+	while (size)
+	{
+		uint32_t fw_dw = *((uint32_t*)&buf[fw_offs]);
+		HAL_FLASH_Unlock();
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, wr_addr_start, fw_dw);
+		FLASH_WaitForLastOperation(1000);
+		HAL_FLASH_Lock();
+		fw_offs += 4;
+		wr_addr_start += 4;
+		size -= 4;
+	}
+}
+
 uint8_t fw_update_check(void)
 {
 	if (f_mount(&USBHFatFS, USBHPath, 1) != FR_OK)
@@ -51,11 +88,11 @@ uint8_t fw_update_check(void)
 	
 	FIL f;
 	uint32_t crc32;
+	uint32_t version;
 	DEBUG_PR("Check updates.\n\r");
 	// check signature
 	if (f_open(&f, "1:/usb_keyb.bin.sign", FA_READ) == FR_OK)
 	{
-		uint32_t version;
 		f_read(&f, &version, sizeof(uint32_t), NULL);
 		f_read(&f, &crc32, sizeof(uint32_t), NULL);
 		f_close(&f);
@@ -68,17 +105,32 @@ uint8_t fw_update_check(void)
 	else
 		return 0;
 	
-	DEBUG_PR("Version mismatched, read firmware...\n\r");
+	DEBUG_PR("Version mismatched:\n\r");
+	DEBUG_PR("\tPrepare flash region...\n\r");
+	prepare_new_fw_region();
 	
-	// read firmware to RAM
+	DEBUG_PR("\tReading firmware...\n\r");
 	if (f_open(&f, "1:/usb_keyb.bin", FA_READ) == FR_OK)
 	{
-		f_read(&f, fw_buf, FW_MAX_SIZE, &fw_size);
+		uint32_t crc_bin = 0;
+		crc32_init();
+		
+		// read firmware to reserved flash
+		uint32_t fw_read = 0;
+		fw_size = 0;
+		uint32_t write_addr = FW_NEW_ADDR_START;
+		do
+		{
+			f_read(&f, fw_read_buf, 1024, &fw_read);
+			crc_bin = crc32_byte(crc_bin, (uint8_t*)fw_read_buf, fw_read);
+			flash_write(fw_read_buf, 1024, write_addr);
+			write_addr += 1024;
+			fw_size += fw_read;
+		} while (fw_read != 0);
 		f_close(&f);
 		
-		uint32_t crc_bin;
-		crc32_init();
-		crc_bin = crc32_byte(0, (uint8_t*)fw_buf, fw_size);
+		// round to 4 bytes
+		fw_size += 4 - (fw_size % 4);
 		
 		// check crc32
 		if (crc_bin != crc32)
@@ -88,7 +140,7 @@ uint8_t fw_update_check(void)
 		}
 		else
 		{
-			DEBUG_PR("Readed firmware (%i bytes)\n\r", fw_size);
+			DEBUG_PR("Readed firmware (%i bytes), version #%i\n\r", fw_size, version);
 			return 1;
 		}
 	}
@@ -98,30 +150,17 @@ uint8_t fw_update_check(void)
 
 inline void flash_erase(void)
 {
-	HAL_FLASH_Unlock();
 	FLASH_EraseInitTypeDef EraseInitStruct;
 	uint32_t error_sector_num = 0;
-  EraseInitStruct.TypeErase = FLASH_TYPEERASE_MASSERASE;
+  EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
   EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
-  EraseInitStruct.Banks = FLASH_BANK_1;
+  EraseInitStruct.Sector = FLASH_SECTOR_0;
+	EraseInitStruct.NbSectors = 4;
+	HAL_FLASH_Unlock();
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
 	HAL_FLASHEx_Erase(&EraseInitStruct, &error_sector_num);
+	FLASH_WaitForLastOperation(1000);
 	HAL_FLASH_Lock();
-}
-
-inline void flash_write(void)
-{
-	uint32_t wr_addr_start;
-	uint32_t fw_offs = 0;
-	
-	while (fw_offs < fw_size)
-	{
-		uint32_t fw_dw = *((uint32_t*)&fw_buf[fw_offs]);
-		wr_addr_start = FLASH_BASE + fw_offs;
-		HAL_FLASH_Unlock();
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, fw_offs, fw_dw);
-		HAL_FLASH_Lock();
-		fw_offs += 4;
-	}
 }
 
 void fw_update(void)
@@ -144,19 +183,25 @@ void fw_update(void)
 	LED_3_ON();
 	
 	flash_erase();
-	flash_write();
+	flash_write((uint8_t*)FW_NEW_ADDR_START, fw_size, FLASH_BASE);
 	
-	HAL_FLASH_Lock();
+	//HAL_FLASH_Lock();
 	
 	int idx = 0;
 	while (1)
 	{
 		++idx;
 		if (idx == 5000000)
+		{
+			LED_1_ON();
+			LED_2_ON();
 			LED_3_ON();
+		}
 		else if (idx == 10000000)
 		{
 			idx = 0;
+			LED_1_OFF();
+			LED_2_OFF();
 			LED_3_OFF();
 		}
 	}
