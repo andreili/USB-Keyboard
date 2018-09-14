a2342285213b4d59203c8e8e5a38e9d95f914225
andreil <andreil499@gmail.com>
2018-09-14 17:14:40 +0300

    Реализован основной финкционал построения матрицы

--- kbd_builder/mainwindow.cpp
+++ kbd_builder/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 #define KEY_ROWS 6
 #define KEY_COLS 21
@@ -18,11 +21,23 @@ QString keys_str[KEY_ROWS][KEY_COLS] =
      {"LShift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "", "", "RShift", "", "Up", "", "1", "2", "3", "Enter"},
      {"LCtrl", "LGUI", "LAlt", "", "", "", "", "Space", "", "", "", "RAlt", "RGUI", "RCtrl", "Left", "Down", "Right", "0", "", ".", ""}};
 
+QString filter = "JSON text files (*.json)";
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_file = "";
+    m_alts[0] = 0x101;
+    m_alts[1] = 0x102;
+    m_alts[2] = 0x104;
+    m_alts[3] = 0x108;
+    m_alts[4] = 0x110;
+    m_alts[5] = 0x120;
+    m_alts[6] = 0x140;
+    m_alts[7] = 0x180;
 
     m_lay = new QGridLayout();
     m_buttons_container = new QButtonGroup();
@@ -37,6 +52,7 @@ MainWindow::MainWindow(QWidget *parent) :
             m_buttons.push_back(btn);
             if (HID_code != -1)
                 m_buttons_container->addButton(btn, HID_code);
+            m_strs[HID_code] = keys_str[i][j];
         }
     }
 
@@ -60,14 +76,7 @@ MainWindow::MainWindow(QWidget *parent) :
         m_rows_menus.push_back(root);
     }
 
-    for (int i=0 ; i<256 ; ++i)
-    {
-        m_keys_row[i] = -1;
-        m_keys_col[i] = -1;
-        m_alts_row[i] = -1;
-        m_alts_col[i] = -1;
-    }
-
+    on_actionNew_triggered();
     ui->centralWidget->setLayout(m_lay);
     updateGeometry();
 }
@@ -94,15 +103,15 @@ void MainWindow::cell_triggered(bool fl)
 
     if (m_cur_HID < 0x100)
     {
-        m_keys_row[m_cur_HID] = cc.first;
-        m_keys_col[m_cur_HID] = cc.second;
+        m_kbd_mtx[m_cur_mtx].m_keys_row[m_cur_HID] = cc.first;
+        m_kbd_mtx[m_cur_mtx].m_keys_col[m_cur_HID] = cc.second;
     }
     else
     {
-        m_alts_row[m_cur_HID - 0x100] = cc.first;
-        m_alts_col[m_cur_HID - 0x100] = cc.second;
+        m_kbd_mtx[0].m_alts_row[m_cur_HID - 0x100] = cc.first;
+        m_kbd_mtx[0].m_alts_col[m_cur_HID - 0x100] = cc.second;
     }
-    m_cur_btn->setStyleSheet("background-color: DarkGreen");
+    update_ui();
 }
 
 void MainWindow::cell_clear(bool fl)
@@ -110,28 +119,239 @@ void MainWindow::cell_clear(bool fl)
     (void)(fl);
     if (m_cur_HID < 0x100)
     {
-        m_keys_row[m_cur_HID] = -1;
-        m_keys_col[m_cur_HID] = -1;
+        m_kbd_mtx[m_cur_mtx].m_keys_row[m_cur_HID] = -1;
+        m_kbd_mtx[m_cur_mtx].m_keys_col[m_cur_HID] = -1;
     }
     else
     {
-        m_alts_row[m_cur_HID - 0x100] = -1;
-        m_alts_col[m_cur_HID - 0x100] = -1;
+        m_kbd_mtx[0].m_alts_row[m_cur_HID - 0x100] = -1;
+        m_kbd_mtx[0].m_alts_col[m_cur_HID - 0x100] = -1;
     }
-    m_cur_btn->setStyleSheet("");
+    update_ui();
 }
 
 void MainWindow::on_actionOpen_triggered()
 {
-    //
+    m_file = QFileDialog::getOpenFileName(this, tr("Укажите файл с раскладкой"), "", filter, &filter);
+    if (m_file.length() > 0)
+    {
+        QFile f(m_file);
+        if (f.open(QIODevice::ReadOnly))
+        {
+            QByteArray json = f.readAll();
+            QJsonDocument doc = QJsonDocument::fromJson(json);
+            QJsonObject root = doc.object();
+            for (int i=0 ; i<2 ; ++i)
+            {
+                QJsonObject mtx = root[QString::number(i)].toObject();
+                for (int j=0 ; j<256 ; ++j)
+                {
+                    if (mtx.contains(QString::number(j)))
+                    {
+                        QJsonObject node = mtx[QString::number(j)].toObject();
+                        m_kbd_mtx[i].m_keys_row[j] = node["r"].toInt();
+                        m_kbd_mtx[i].m_keys_col[j] = node["c"].toInt();
+                    }
+                    if (mtx.contains(QString::number(0x100 + j)))
+                    {
+                        QJsonObject node = mtx[QString::number(0x100 + j)].toObject();
+                        m_kbd_mtx[0].m_alts_row[j] = node["r"].toInt();
+                        m_kbd_mtx[0].m_alts_col[j] = node["c"].toInt();
+                    }
+                }
+            }
+            on_actionLat_triggered();
+        }
+        f.close();
+    }
 }
 
 void MainWindow::on_actionSave_triggered()
 {
-    //
+    save_to_file();
 }
 
 void MainWindow::on_actionSave_as_triggered()
 {
-    //
+    m_file = QFileDialog::getSaveFileName(this, tr("Укажите файл с раскладкой"), "", filter, &filter);
+    if (m_file.length() > 0)
+        save_to_file();
+}
+
+void MainWindow::on_actionLat_triggered()
+{
+    m_cur_mtx = 0;
+    ui->actionLat->setChecked(true);
+    ui->actionCyr->setChecked(false);
+    update_ui();
+}
+
+void MainWindow::on_actionCyr_triggered()
+{
+    m_cur_mtx = 1;
+    ui->actionLat->setChecked(false);
+    ui->actionCyr->setChecked(true);
+    update_ui();
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    for (int i=0 ; i<256 ; ++i)
+    {
+        m_kbd_mtx[0].m_keys_row[i] = -1;
+        m_kbd_mtx[0].m_keys_col[i] = -1;
+        m_kbd_mtx[0].m_alts_row[i] = -1;
+        m_kbd_mtx[0].m_alts_col[i] = -1;
+
+        m_kbd_mtx[1].m_keys_row[i] = -1;
+        m_kbd_mtx[1].m_keys_col[i] = -1;
+        m_kbd_mtx[1].m_alts_row[i] = -1;
+        m_kbd_mtx[1].m_alts_col[i] = -1;
+    }
+
+    on_actionLat_triggered();
+}
+
+#define TO_MTX(arr_row, arr_col, idx) \
+    int row = arr_row[idx]; \
+    int col = arr_col[idx]; \
+    if ((row == -1) || (col == -1)) \
+    { \
+        row = 15; \
+        col = 15; \
+    } \
+    ln += "KMX(" + QString::number(row) + "," + \
+            QString::number(col) + "),";
+
+#define SAVE_MTX(idx, kname) \
+    lst.push_back(QString("const uint8_t kbd_" + name + "_" + kname + "[256] = {")); \
+    for (int i=0 ; i<32 ; ++i) \
+    { \
+        ln = "\t\t\t\t\t\t"; \
+        for (int j=0 ; j<8 ; ++j) \
+        { \
+            TO_MTX(m_kbd_mtx[idx].m_keys_row, m_kbd_mtx[idx].m_keys_col, i*16+j) \
+        } \
+        if (i == 31) \
+        { \
+            ln.remove(ln.size() - 1, 1); \
+            ln += "};"; \
+        } \
+        lst.push_back(ln); \
+    }
+
+void MainWindow::on_actionExport_header_triggered()
+{
+    if (m_file.length() == 0)
+        return;
+
+    QStringList fns = m_file.split("/");
+    QString name = fns[fns.size() - 1];
+    name = name.left(name.lastIndexOf('.'));
+
+    QStringList lst;
+    QString ln;
+
+    lst.push_back(QString("#ifndef _KBD_" + name.toUpper() + "_DATA_H_"));
+    lst.push_back(QString("#define _KBD_" + name.toUpper() + "_DATA_H_"));
+    lst.push_back("");
+    lst.push_back("#include <inttypes.h>");
+    lst.push_back("");
+    SAVE_MTX(0, "lat");
+    SAVE_MTX(1, "rus");
+    ln = "const uint8_t kbd_" + name + "_f[4] = {";
+    for (int i=0 ; i<8 ; ++i)
+    {
+        TO_MTX(m_kbd_mtx[0].m_alts_row, m_kbd_mtx[0].m_alts_col, (m_alts[i] - 0x100))
+    }
+    ln.remove(ln.size() - 1, 1);
+    ln += "};";
+    lst.push_back(ln);
+
+    lst.push_back(QString("kbd_matrix_t kbd_rk86 = {kbd_" + name + "_lat, kbd_" + name + "_rus, kbd_" + name + "_f};"));
+
+    name = m_file.left(m_file.lastIndexOf('.')) + ".h";
+    QFile f(name);
+    if (f.open(QIODevice::WriteOnly))
+        for (int i=0 ; i<lst.length() ; ++i)
+            f.write((lst[i] + "\n").toLatin1());
+    f.close();
+}
+
+void MainWindow::save_to_file()
+{
+    if (m_file.length() == 0)
+        return;
+
+    QJsonDocument doc;
+    QJsonObject root;
+    for (int i=0 ; i<2 ; ++i)
+    {
+        QJsonObject mtx;
+        for (int j=0 ; j<256 ; ++j)
+        {
+            if (m_kbd_mtx[i].m_keys_row[j] != -1)
+            {
+                QJsonObject node;
+                node["r"] = m_kbd_mtx[i].m_keys_row[j];
+                node["c"] = m_kbd_mtx[i].m_keys_col[j];
+                mtx[QString::number(j)] = node;
+            }
+            if (m_kbd_mtx[0].m_alts_row[j] != -1)
+            {
+                QJsonObject node;
+                node["r"] = m_kbd_mtx[0].m_alts_row[j];
+                node["c"] = m_kbd_mtx[0].m_alts_col[j];
+                mtx[QString::number(j + 0x100)] = node;
+            }
+        }
+        root[QString::number(i)] = mtx;
+    }
+    doc.setObject(root);
+    QByteArray json = doc.toJson();
+
+    QFile f(m_file);
+    if (f.open(QIODevice::WriteOnly))
+        f.write(json);
+    f.close();
+}
+
+void MainWindow::update_ui()
+{
+    for (int i=0 ; i<256 ; ++i)
+    {
+        QAbstractButton* btn = m_buttons_container->button(i);
+        int row = m_kbd_mtx[m_cur_mtx].m_keys_row[i];
+        int col = m_kbd_mtx[m_cur_mtx].m_keys_col[i];
+        if (btn != nullptr)
+        {
+            if (row != -1)
+            {
+                btn->setStyleSheet("background-color: DarkGreen");
+                btn->setText(m_strs[i] + "[" + QString::number(row) + ":" + QString::number(col) + "]");
+            }
+            else
+            {
+                btn->setStyleSheet("");
+                btn->setText(m_strs[i]);
+            }
+        }
+
+        btn = m_buttons_container->button(0x100 + i);
+        row = m_kbd_mtx[0].m_alts_row[i];
+        col = m_kbd_mtx[0].m_alts_col[i];
+        if (btn != nullptr)
+        {
+            if (row != -1)
+            {
+                btn->setStyleSheet("background-color: DarkGreen");
+                btn->setText(m_strs[0x100 + i] + "[" + QString::number(row) + ":" + QString::number(col) + "]");
+            }
+            else
+            {
+                btn->setStyleSheet("");
+                btn->setText(m_strs[0x100 + i]);
+            }
+        }
+    }
 }
--- kbd_builder/mainwindow.h
+++ kbd_builder/mainwindow.h
@@ -12,6 +12,14 @@ namespace Ui {
 class MainWindow;
 }
 
+typedef struct
+{
+    int                 m_keys_row[256];
+    int                 m_keys_col[256];
+    int                 m_alts_row[256];
+    int                 m_alts_col[256];
+} kbd_mtx_t;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -30,6 +38,14 @@ private slots:
 
     void on_actionSave_as_triggered();
 
+    void on_actionLat_triggered();
+
+    void on_actionCyr_triggered();
+
+    void on_actionNew_triggered();
+
+    void on_actionExport_header_triggered();
+
 private:
     Ui::MainWindow      *ui;
     QButtonGroup        *m_buttons_container;
@@ -39,14 +55,19 @@ private:
     QAction             *m_title;
     QList<QMenu*>       m_rows_menus;
 
+    QString             m_file;
+
     QAbstractButton*    m_cur_btn;
     int                 m_cur_HID;
-    int                 m_keys_row[256];
-    int                 m_keys_col[256];
-    int                 m_alts_row[256];
-    int                 m_alts_col[256];
+    int                 m_cur_mtx;
+    kbd_mtx_t           m_kbd_mtx[2];
 
-    QMap<QAction*,QPair<int,int>> m_cells;
+    QMap<QAction*,QPair<int,int>>   m_cells;
+    QMap<int,QString>               m_strs;
+    QMap<int,int>                   m_alts;
+
+    void save_to_file();
+    void update_ui();
 };
 
 #endif // MAINWINDOW_H
